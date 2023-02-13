8f6b368f3370f7d69fb4995aaf57729907844471
Carlos Diaz <c.diazadriann@gmail.com>
2023-02-13 11:18:34 -0400

    Curso FIn

--- src/components/messages-ws/messages-ws.service.ts
+++ src/components/messages-ws/messages-ws.service.ts
@@ -25,7 +25,7 @@ export class MessagesWsService {
     const user = await this.userRepository.findOneBy({ id: userId });
     if (!user) throw new Error('User not Found');
     if (!user.isActive) throw new Error('User not Active');
-
+    this.checkUserConnection(user);
     this.connectedClients[client.id] = {
       socket: client,
       user: user,
@@ -43,5 +43,15 @@ export class MessagesWsService {
 
   getUserFullName(socketId: string) {
     return this.connectedClients[socketId].user.fullName;
+  }
+
+  private checkUserConnection(user: User) {
+    for (const clientID of Object.keys(this.connectedClients)) {
+      const connectedClient = this.connectedClients[clientID];
+      if (connectedClient.user.id === user.id) {
+        connectedClient.socket.disconnect();
+        break;
+      }
+    }
   }
 }
